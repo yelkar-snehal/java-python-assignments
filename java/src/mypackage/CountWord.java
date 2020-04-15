bcb4ec27b70d3ac22f6a1534631b15dc57f76ce3
yelkar-snehal <31803022+yelkar-snehal@users.noreply.github.com>
2020-04-15 18:14:27 +0530

    Develop (#8)
    * Add soln to word count
    
    * Modify word count code to handle empty str case

---
+++ java/src/mypackage/CountWord.java
@@ -0,0 +1,55 @@
+package mypackage;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class CountWord {
+
+	public static void displayWordCount(String str) {
+
+		HashMap<String, Integer> mp = new HashMap<>();
+
+		// remove leading trailing spaces by trim
+		// split string by spaces
+		String[] words = str.trim().split("\\s+");
+
+		// System.out.println( words[0] + words.length );
+
+		// only spaces are entered or empty string
+		if ((words[0].equals(""))) {
+			System.out.println("Invalid or empty string!");
+			return;
+		}
+
+		for (String word : words) {
+			if (mp.containsKey(word)) {
+				// check if word is already present
+				// and increment it's value
+				mp.put(word, mp.get(word) + 1);
+			} else {
+				// add the word if not already present
+				mp.put(word, 1);
+			}
+		}
+
+		// display words and their counts
+		mp.forEach((key, value) -> System.out.printf("Word: %s, Count: %d\n", key, value));
+		System.out.println("Total number of words: " + words.length);
+
+	}
+
+	public static void main(String[] args) {
+
+		String str = "";
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a string");
+		str = sc.nextLine();
+
+		// System.out.print(str);
+		displayWordCount(str);
+
+		// close resources
+		sc.close();
+	}
+}
