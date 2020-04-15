84fdfd8f8b9e2c49d9215ffb195dcabf7ea3633e
Snehal Yelkar <yelkarsnehal9@gmail.com>
2020-04-15 19:02:46 +0530

    Add soln for bubblesort

--- java/src/mypackage/ArrayInput.java
+++ java/src/mypackage/ArrayInput.java
@@ -4,35 +4,43 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ArrayInput {
-	
-	public static int[] setArray(Scanner sc) {
-		
-		int n = 0;
-		
-		// user input
-				System.out.println("Enter number of elemnts in the array");
-				try {
-					n = sc.nextInt();
-				} catch (InputMismatchException | NumberFormatException e) {
-					System.out.println("That's not a number!\nThis execption occured\n" + e);
-					System.exit(1);
-				}
 
-				int[] arr = new int[n];
-				// array init
-				for (int i = 0; i < n; i++) {
-					System.out.printf("Enter element %d: \n", i + 1);
-					try {
-						arr[i] = sc.nextInt();
-					} catch (InputMismatchException | NumberFormatException e) {
-						System.out.println("That's not a number!\nThis execption occured\n" + e);
-						System.exit(1);
-					}
-				}
-				
-				// close resources
-				// sc.close();
-				
-				return arr;
+	public static int[] setArray(Scanner sc) {
+
+		int n = 0;
+
+		// initialize array
+		System.out.println("Enter number of elemnts in the array");
+		try {
+			n = sc.nextInt();
+		} catch (InputMismatchException | NumberFormatException e) {
+			System.out.println("That's not a number!\nThis execption occured\n" + e);
+			System.exit(1);
+		}
+
+		int[] arr = new int[n];
+		// array init
+		for (int i = 0; i < n; i++) {
+			System.out.printf("Enter element %d: \n", i + 1);
+			try {
+				arr[i] = sc.nextInt();
+			} catch (InputMismatchException | NumberFormatException e) {
+				System.out.println("That's not a number!\nThis execption occured\n" + e);
+				System.exit(1);
+			}
+		}
+
+		// close resources
+		// sc.close();
+
+		return arr;
+	}
+
+	// display array contents
+	public static void getArray(int[] arr) {
+		for (int i : arr) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
 	}
 }
