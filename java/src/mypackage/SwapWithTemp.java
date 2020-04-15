13c1aafbe38c58b2f2ad4e54ac03ffb0daf6c7a1
Snehal Yelkar <yelkarsnehal9@gmail.com>
2020-04-15 12:00:25 +0530

    Modify swap to add modularity

--- java/src/mypackage/SwapWithTemp.java
+++ java/src/mypackage/SwapWithTemp.java
@@ -6,6 +6,20 @@ import java.io.InputStreamReader;
 
 public class SwapWithTemp {
 
+	public static int[] swapNumbers(int no1, int no2) {
+		// declare a temp variable'
+		int temp = 0;
+		// swapping logic
+		temp = no1;
+		no1 = no2;
+		no2 = temp;
+
+		// return new values
+		// as func is call by value
+		int[] ret = { no1, no2 };
+		return ret;
+	}
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 
 		int no1, no2;
@@ -17,12 +31,9 @@ public class SwapWithTemp {
 
 		System.out.printf("The two numbers you entered are %d and %d\n", no1, no2);
 
-		// declare a temp variable'
-		int temp = 0;
-		// swapping logic
-		temp = no1;
-		no1 = no2;
-		no2 = temp;
+		int[] ret = swapNumbers(no1, no2);
+		no1 = ret[0];
+		no2 = ret[1];
 
 		System.out.printf("The two numbers after swapping are %d and %d", no1, no2);
 	}
