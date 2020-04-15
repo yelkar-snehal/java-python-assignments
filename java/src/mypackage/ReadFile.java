d5a9e78500ec8ed31b6a0944b354f3a9664abc41
Snehal Yelkar <yelkarsnehal9@gmail.com>
2020-04-15 14:09:05 +0530

    Add solution to readfile

---
+++ java/src/mypackage/ReadFile.java
@@ -0,0 +1,42 @@
+package mypackage;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ReadFile {
+	
+	public static String displayFileContents(File file) {
+		String fileContents = "";
+		Scanner sc;
+		
+		// create scanner obj of file 
+		try {
+			sc = new Scanner(file);
+			//iterate over scanner object to 
+			// append file contents
+			while(sc.hasNextLine()) {
+				fileContents += sc.nextLine()+"\n";
+			}
+		} catch(FileNotFoundException e) {
+			System.out.println("File not found!");
+		}
+		
+		return fileContents;
+	}
+	
+	public static void main(String[] args) {
+
+		String filePath = "";
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter path to a file");
+		filePath = sc.nextLine();
+
+		System.out.println("File Contents: \n" + displayFileContents(new File(filePath)));
+
+		// close resources
+		sc.close();
+
+	}
+}
