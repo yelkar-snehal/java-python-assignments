2c9ee8d53ed0d2cc35cdd6211f34f9c46a34a913
Snehal Yelkar <yelkarsnehal9@gmail.com>
2020-04-15 18:52:26 +0530

    Move method for taking array input into a separate file

--- java/src/mypackage/LinearSearch.java
+++ java/src/mypackage/LinearSearch.java
@@ -20,30 +20,11 @@ public class LinearSearch {
 
 	public static void main(String[] args) {
 
-		int n = 0, ele = 0;
+		int ele = 0;
 
-		// user input
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Enter number of elemnts in the array");
-		try {
-			n = sc.nextInt();
-		} catch (InputMismatchException | NumberFormatException e) {
-			System.out.println("That's not a number!\nThis execption occured\n" + e);
-			System.exit(1);
-		}
-
-		int[] arr = new int[n];
-		// array init
-		for (int i = 0; i < n; i++) {
-			System.out.printf("Enter element %d: \n", i + 1);
-			try {
-				arr[i] = sc.nextInt();
-			} catch (InputMismatchException | NumberFormatException e) {
-				System.out.println("That's not a number!\nThis execption occured\n" + e);
-				System.exit(1);
-			}
-		}
+		int[] arr = ArrayInput.setArray(sc);
 
 		// scan element to be searched
 		System.out.println("Enter element to be searched");
