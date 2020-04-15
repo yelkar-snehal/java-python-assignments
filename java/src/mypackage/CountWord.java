e767cb94bd9d80c459a68f26542df4faad7dfd5b
Snehal Yelkar <yelkarsnehal9@gmail.com>
2020-04-15 17:36:46 +0530

    And soln to word count

---
+++ java/src/mypackage/CountWord.java
@@ -0,0 +1,48 @@
+package mypackage;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class CountWord {
+
+	public static void displayWordCount(String str) {
+
+		HashMap<String, Integer> mp = new HashMap<>();
+
+		// remove leading trailing spaces by trim
+		// split string by spaces
+		String[] words = str.trim().split("\\s+");
+		
+		//System.out.println(words.length);
+		
+		for (String word : words) {
+			if (mp.containsKey(word)) {
+				// check if word is already present
+				// and increment it's value
+				mp.put(word, mp.get(word) + 1);
+			} else {
+				// add the word if not already present
+				mp.put(word, 1);
+			}
+		}
+
+		// display words and their counts
+		mp.forEach((key, value) -> System.out.printf("Word: %s, Count: %d\n", key, value));
+		System.out.println("Total number of words: " + mp.values());
+
+	}
+
+	public static void main(String[] args) {
+
+		String str = "";
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a string");
+		str = sc.nextLine();
+
+		displayWordCount(str);
+
+		// close resources
+		sc.close();
+	}
+}
