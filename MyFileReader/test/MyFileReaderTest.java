117f9bd80df897e58f6a396c2d833fb75a6f2711
yelkar-snehal <31803022+yelkar-snehal@users.noreply.github.com>
2020-04-16 23:06:55 +0530

    Separate main and logic classes

--- MyFileReader/test/MyFileReaderTest.java
+++ MyFileReader/test/MyFileReaderTest.java
@@ -1,12 +1,30 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 class MyFileReaderTest {
-
+	
+	@TempDir
+	static File dirPath;
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testCreateFile() {
+		// System.out.println(dirPath.getAbsolutePath());
+		File a = new File(dirPath, "demo.txt");
+		try {
+			assertTrue(a.createNewFile());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-
+	
+	@Test
+	void testCreateDir() {
+		File b = new File(dirPath, "demoDir");
+		assertTrue(b.mkdir());
+	}
 }
