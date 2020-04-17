1e4beb839cbfaa791cd122fd4a16cf0aa53eb06a
Snehal Yelkar <yelkarsnehal9@gmail.com>
2020-04-17 23:26:04 +0530

    Modify display methods and add comments

--- MyFileReader/src/com/example/myfilereader/DemoMyFileReader.java
+++ MyFileReader/src/com/example/myfilereader/DemoMyFileReader.java
@@ -3,8 +3,11 @@ package com.example.myfilereader;
 import java.io.File;
 
 public class DemoMyFileReader {
+
 	public static void main(String[] args) {
 
+		// check whether file path is provided
+		// exit the program if no argument is provided
 		if (0 == args.length) {
 			System.out.println("No argument provided!");
 			System.exit(1);
@@ -12,15 +15,26 @@ public class DemoMyFileReader {
 
 		File file = new File(args[0]);
 
-		// check if provided arg is dir
+		// check if provided argument is a directory
 		if (!file.isDirectory()) {
 			System.out.println("Provided argument isn't a valid directory!");
 			System.exit(1);
 		}
 
+		// create an object of MyFileReader class
 		MyFileReader obj = new MyFileReader(file);
+
+		// mapping is necessary as the resulted
+		// hashmap is used for all operations
 		obj.mapFiles();
-		obj.displayFiles();
+
+		// obj.displayFiles();
+
+		// display files in ascending order
 		obj.displayFiles(true);
+
+		// check if given sequence is present in any file
+		obj.displayScanFiles("hello", "txt");
+
 	}
 }
--- MyFileReader/src/com/example/myfilereader/MyFileReader.java
+++ MyFileReader/src/com/example/myfilereader/MyFileReader.java
@@ -13,9 +13,16 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class MyFileReader {
+
 	// class variables common to most methods
+
+	// to store root directory path
 	File dirPath;
+
+	// to store all available files excluding directories
 	ArrayList<File> files;
+
+	// to store mapped filename, path and sizes
 	HashMap<String, Long> mp;
 
 	// constructor to initialize path to directory
@@ -25,7 +32,7 @@ public class MyFileReader {
 		mp = new HashMap<String, Long>();
 	}
 
-	// sets the list of files including directories
+	// set the list of all files excluding directories
 	public void setFiles(File path) {
 		File[] arrFiles = path.listFiles();
 		for (File file : arrFiles) {
@@ -35,16 +42,20 @@ public class MyFileReader {
 		}
 	}
 
+	// get the sorted list of all stored files
 	public ArrayList<File> getFiles() {
 		Collections.sort(files);
 		return files;
 	}
 
-	// get file size default bytes
+	// get file size
+	// default bytes
 	public long getFileSize(File file) {
 		return file.length();
 	}
 
+	// get file sizes for all stored files
+	// return sorted array list of sizes
 	public ArrayList<Long> getFileSizes() {
 		ArrayList<Long> fileSizes = new ArrayList<Long>();
 		for (File file : files) {
@@ -54,8 +65,10 @@ public class MyFileReader {
 		return fileSizes;
 	}
 
+	// store file name, path and size in a hashmap
+	// invoked at 1st recursive call
+	// take root directory's path as no input is provided
 	public void mapFiles() {
-
 		// 1st call
 		setFiles(dirPath);
 		// files = getFiles();
@@ -71,13 +84,16 @@ public class MyFileReader {
 				// recursive traverse directory
 				mapFiles(file);
 			} else {
+				// add to hashmap if file isn't of type dir
 				mp.put(file.getAbsolutePath() + " " + file.getName(), getFileSize(file));
 			}
 		}
 	}
 
+	// overloaded method
+	// invoked when a parameter of type File is passed
+	// store file name, path and size in a hashmap
 	public void mapFiles(File path) {
-
 		// recursive call
 		setFiles(path);
 		File[] currentDir = path.listFiles();
@@ -87,35 +103,42 @@ public class MyFileReader {
 				// recursive traverse directory
 				mapFiles(file);
 			} else {
+				// add to hashmap if file isn't of type dir
 				mp.put(file.getAbsolutePath() + " " + file.getName(), getFileSize(file));
 			}
 		}
-
-		// return mp;
-
 	}
 
+	// get mapped file data
 	public HashMap<String, Long> getMappedFiles() {
 		return this.mp;
 	}
 
+	// print file data in any order
 	public void displayFiles() {
 		// System.out.println(mp.size());
+
+		// iterate over map if the same isn't empty
 		if (0 != files.size()) {
 			mp.forEach((file, s) -> System.out.println(file + " " + s));
 		}
 
 	}
 
+	// sort hashmap by values
+	// sorts mapped data by file size
 	public HashMap<String, Long> getSortedFiles(HashMap<String, Long> hm) {
+		// get list from hashmap
 		List<Map.Entry<String, Long>> list = new LinkedList<Map.Entry<String, Long>>(hm.entrySet());
 
+		// sort list by values
 		Collections.sort(list, new Comparator<Map.Entry<String, Long>>() {
 			public int compare(Map.Entry<String, Long> obj1, Map.Entry<String, Long> obj2) {
 				return (obj1.getValue()).compareTo(obj2.getValue());
 			}
 		});
 
+		// copy list to another hashmap
 		HashMap<String, Long> temp = new LinkedHashMap<String, Long>();
 		for (Map.Entry<String, Long> map : list) {
 			temp.put(map.getKey(), map.getValue());
@@ -123,18 +146,30 @@ public class MyFileReader {
 		return temp;
 	}
 
+	// print file data in ascending order of file size
 	public void displayFiles(boolean ascending) {
 
 		if (0 != files.size()) {
 
 			HashMap<String, Long> sortedMap = getSortedFiles(this.mp);
 
+			System.out.println(
+					"---------------------------------------------------------------------------------------------");
 			System.out.println("Displaying files in ascending order...");
-			sortedMap.forEach((file, s) -> System.out.println(file + " " + s));
+			sortedMap.forEach((file, s) -> {
+				// splits path and filename
+				String[] nameAndPath = file.split(" ");
+
+				System.out.println("\nFilename: " + nameAndPath[1] + "\nPath: " + nameAndPath[0] + "\nSize: " + s
+						+ " bytes or " + s / 1024 + " kB");
+			});
+			System.out.println(
+					"---------------------------------------------------------------------------------------------");
 
 		}
 	}
 
+	// get file's extension
 	public String getFileExtension(String name) {
 		String ext = "";
 
@@ -147,12 +182,18 @@ public class MyFileReader {
 		return ext;
 	}
 
+	// get mapped file data provided the file type
 	public HashMap<String, Long> filterFiles(String ext) {
 		HashMap<String, Long> filteredFiles = new HashMap<String, Long>();
 
 		mp.forEach((file, s) -> {
+			// splits path and filename
 			String[] pathAndName = file.split(" ");
+
+			// get file extensions of all mapped files
+			// compare with provided extension
 			if (ext.equals(getFileExtension(pathAndName[1]))) {
+				// add to filtered op if extension matches
 				filteredFiles.put(file, s);
 			}
 		});
@@ -162,39 +203,71 @@ public class MyFileReader {
 
 	}
 
+	// search files for a particular sequence
+	// takes sequence and file type as input
+	// returns file data along with the line numbers,
+	// where the sequence occurs
 	public HashMap<String, ArrayList<Integer>> scanFiles(String word, String ext) {
 		HashMap<String, ArrayList<Integer>> scannedFiles = new HashMap<String, ArrayList<Integer>>();
 
+		// get sorted map for provided extension
 		HashMap<String, Long> filteredFiles = filterFiles(ext);
 
 		filteredFiles.forEach((file, s) -> {
+			// splits path and filename
 			String[] pathAndName = file.split(" ");
 
 			int count = 0;
 			Scanner sc;
 			try {
 				sc = new Scanner(new File(pathAndName[0]));
+				// generate new arraylist for each file
 				ArrayList<Integer> lineNumbers = new ArrayList<Integer>();
 
+				// iterate till last line in the file
 				while (sc.hasNextLine()) {
+					// increment line number
 					count++;
 
-					if (sc.nextLine().contains(word)) {
-
+					// case insensitive search for provided sequence
+					if (sc.nextLine().toLowerCase().contains(word.toLowerCase())) {
+						// add line numbers to arraylist
 						lineNumbers.add(count);
-						scannedFiles.put(pathAndName[0], lineNumbers);
-
+						// add file
+						scannedFiles.put(file + " " + s, lineNumbers);
 					}
-
 				}
 			} catch (FileNotFoundException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-
 		});
 
 		return scannedFiles;
+	}
+
+	// print search results in ascending order of file size
+	public void displayScanFiles(String word, String ext) {
+
+		HashMap<String, ArrayList<Integer>> scannedFiles = scanFiles(word, ext);
+
+		System.out.println(
+				"---------------------------------------------------------------------------------------------");
+		System.out.printf("The word '%s' occured in these files: \n", word);
+		if (0 == scannedFiles.size()) {
+			System.out.println("No Search Results!");
+		} else {
+			scannedFiles.forEach((file, lines) -> {
+				// splits path and filename
+				String[] nameAndPath = file.split(" ");
+
+				System.out.println("\nFilename: " + nameAndPath[1] + "\nPath: " + nameAndPath[0] + "\nLines: " + lines
+						+ "\nSize: " + nameAndPath[2] + " bytes or " + Long.parseLong(nameAndPath[2]) / 1024 + " kB");
+			});
+		}
+		System.out.println(
+				"---------------------------------------------------------------------------------------------");
+
 	}
 
 }
