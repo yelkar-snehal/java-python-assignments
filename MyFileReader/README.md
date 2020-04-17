03f08b1a26874682d1f31a80c68531bd0f15f3a6
yelkar-snehal <31803022+yelkar-snehal@users.noreply.github.com>
2020-04-17 23:38:16 +0530

    Develop (#17)
    * Modify display methods and add comments
    
    * Create README.md

---
+++ MyFileReader/README.md
@@ -0,0 +1,19 @@
+# MyFileReader
+
+## A java application for nested file reading
+
+#### Functionalites
+
+1. Get all files present in the provided directory by iterating the same recursively.
+
+2. Store all files in a HashMap for fast access.
+
+3. Get sizes of all files in the provided directory.
+
+4. Sort files based on their sizes.
+
+5. Filter files by the provided file type.
+
+6. Search for a sequence in all files.
+
+7. List all line numbers if a sequence is matched.
--- MyFileReader/test/MyFileReaderTest.java
+++ MyFileReader/test/MyFileReaderTest.java
@@ -22,13 +22,17 @@ class MyFileReaderTest {
 
 		files = new ArrayList<File>();
 		dirPath = new File("/home/synerzip/Documents/demo");
+		// array all of files in dirPath includes nested
 		File[] allFiles = { new File("/home/synerzip/Documents/demo/demo.txt"),
 				new File("/home/synerzip/Documents/demo/block.c"), new File("/home/synerzip/Documents/demo/mexe"),
 				new File("/home/synerzip/Documents/demo/csv/TestTakers.csv"),
 				new File("/home/synerzip/Documents/demo/csv/MarksScored.csv"),
-				new File("/home/synerzip/Documents/demo/csv/Analysis.csv") };
+				new File("/home/synerzip/Documents/demo/csv/Analysis.csv"),
+				new File("/home/synerzip/Documents/demo/csv/hello.txt") };
 
 		Collections.addAll(files, allFiles);
+
+		// get sorted files to compare
 		Collections.sort(files);
 		fileReaderObj = new MyFileReader(dirPath);
 		fileReaderObj.mapFiles();
@@ -44,7 +48,7 @@ class MyFileReaderTest {
 	// @ValueSource(longs = {files[0].length(), files[1].length()})
 	@Test
 	public void testGetFileSizes() {
-		Long[] arrfileSizes = { (long) 71, (long) 97, (long) 6708, (long) 8304, (long) 14206, (long) 64517 };
+		Long[] arrfileSizes = { (long) 54, (long) 71, (long) 97, (long) 6708, (long) 8304, (long) 14206, (long) 64517 };
 		ArrayList<Long> fileSizes = new ArrayList<Long>();
 		Collections.addAll(fileSizes, arrfileSizes);
 		assertEquals(fileSizes, fileReaderObj.getFileSizes());
@@ -52,7 +56,9 @@ class MyFileReaderTest {
 
 	@Test
 	public void testGetSortedFiles() {
+		// hashmap of sorted files by size
 		HashMap<String, Long> map = new HashMap<String, Long>();
+		map.put("/home/synerzip/Documents/demo/csv/hello.txt hello.txt", (long) 54);
 		map.put("/home/synerzip/Documents/demo/demo.txt demo.txt", (long) 71);
 		map.put("/home/synerzip/Documents/demo/block.c block.c", (long) 97);
 		map.put("/home/synerzip/Documents/demo/csv/MarksScored.csv MarksScored.csv", (long) 6708);
@@ -66,7 +72,9 @@ class MyFileReaderTest {
 
 	@Test
 	public void testFilterFiles() {
+		// hashmap of filtered files by provided extension
 		HashMap<String, Long> map = new HashMap<String, Long>();
+		map.put("/home/synerzip/Documents/demo/csv/hello.txt hello.txt", (long) 54);
 		map.put("/home/synerzip/Documents/demo/demo.txt demo.txt", (long) 71);
 
 		assertEquals(map, fileReaderObj.filterFiles("txt"));
@@ -74,13 +82,21 @@ class MyFileReaderTest {
 
 	@Test
 	public void testScanFiles() {
+		// hashmaps of scanned files for given extension and sequence
 		HashMap<String, ArrayList<Integer>> map = new HashMap<String, ArrayList<Integer>>();
-		ArrayList<Integer> lineNumbers = new ArrayList<Integer>();
-		lineNumbers.add(2);
-		lineNumbers.add(4);
-		lineNumbers.add(5);
-		lineNumbers.add(6);
-		map.put("/home/synerzip/Documents/demo/demo.txt", lineNumbers);
+		ArrayList<Integer> lineNumbers1 = new ArrayList<Integer>();
+		lineNumbers1.add(2);
+		lineNumbers1.add(4);
+		lineNumbers1.add(5);
+		lineNumbers1.add(6);
+		ArrayList<Integer> lineNumbers2 = new ArrayList<Integer>();
+		lineNumbers2.add(1);
+		lineNumbers2.add(2);
+		lineNumbers2.add(4);
+		lineNumbers2.add(5);
+		lineNumbers2.add(6);
+		map.put("/home/synerzip/Documents/demo/csv/hello.txt hello.txt 54", lineNumbers2);
+		map.put("/home/synerzip/Documents/demo/demo.txt demo.txt 71", lineNumbers1);
 
 		assertEquals(map, fileReaderObj.scanFiles("hello", "txt"));
 	}
